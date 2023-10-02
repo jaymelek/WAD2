af471fcbf0e37025ecfab8c4f283023c984b7626
jaymelek <112159350+jaymelek@users.noreply.github.com>
2023-10-02 18:57:17 +0800

    display data

--- wad2-maki/src/views/testPage.vue
+++ wad2-maki/src/views/testPage.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div>
     <h1>Data from Firebase</h1>
@@ -8,26 +6,35 @@
 </template>
 
 <script>
-
+import { ref, onMounted } from 'vue'; // Import Vue composition API functions
 import { app } from "../firebase/firebase"; 
-import { getDatabase, ref, get } from 'firebase/database';
+import { getDatabase, ref as dbRef, get } from 'firebase/database';
 
-const db = getDatabase(app); // Use the 'app' object to get the database instance
-const databaseRef = ref(db, 'games');
+export default {
+  setup() {
+    const firebaseData = ref(null);
 
-// Read data from the database
-get(databaseRef)
-  .then((snapshot) => {
-    if (snapshot.exists()) {
-      const data = snapshot.val();
-      return data
-    } else {
-      console.log('No data available');
-    }
-  })
-  .catch((error) => {
-    console.error('Error reading data from Realtime Database:', error);
-  }
-);
+    onMounted(async () => {
+      try {
+        const db = getDatabase(app);
+        const databaseRef = dbRef(db, 'games');
 
+        const snapshot = await get(databaseRef);
+
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          firebaseData.value = data; // Update the data property with fetched data
+        } else {
+          console.log('No data available');
+        }
+      } catch (error) {
+        console.error('Error reading data from Realtime Database:', error);
+      }
+    });
+
+    return {
+      firebaseData,
+    };
+  },
+};
 </script>
