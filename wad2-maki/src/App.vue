6c5b1a2b9e4afbeabe452e14aced3d4d8e226231
danixlc00 <bigcow315@gmail.com>
2023-10-04 12:32:47 +0800

    updated

--- wad2-maki/src/App.vue
+++ wad2-maki/src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import Navbar from "../components/Navbar.vue";
+import Navbar from "./components/navBar.vue";
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default {
