f11aa895f68b5aa4d18e320a65a36a008bea3266
beatbeat8 <beatricegan.2021@smu.edu.sg>
2023-10-17 14:02:29 +0800

    clean up app.vue

--- wad2-maki/src/App.vue
+++ wad2-maki/src/App.vue
@@ -4,22 +4,17 @@
 <template>
   <navBar />
   <router-view></router-view>
-  <!-- <viewGame /> -->
 </template>
 
 <script>
 import navBar from "./components/navBar.vue";
 import 'bootstrap/dist/css/bootstrap.css';
-// to be deleted
-// import viewGame from "./views/viewGame.vue";
 
 export default {
   name: 'App',
   components: {
     navBar
   },
-  // methods: {
-  // },
 
 }
 </script>
@@ -29,7 +24,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
 }
 
