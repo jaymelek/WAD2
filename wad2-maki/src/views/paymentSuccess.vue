17a4a17098a4ed2e5087bf85e46cad9a05a772d2
jaymelek <112159350+jaymelek@users.noreply.github.com>
2023-10-31 22:41:05 +0800

    payment success

--- wad2-maki/src/views/paymentSuccess.vue
+++ wad2-maki/src/views/paymentSuccess.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid text-center">
       <h1>Payment Success!</h1>
       <p>Thank you for your payment!</p>
-      <p>Click <router-link :to="{ name: 'landingPage' }">here</router-link> to go back to the home page.</p>
+      <p>Click <router-link :to="{ name: 'landingPage' }" class="router">here</router-link> to go back to the home page.</p>
     </div>
 
   </div>
@@ -12,13 +12,27 @@
 </template>
   
 <style scoped>
-    .container-fluid {
-      background-color: #f4f4f4;
-      padding: 20px;
-      border-radius: 10px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-      color: #070F5F;
-    }
+.container-fluid{
+  border: 1px solid #286fb4;
+  border-radius: 5px;
+  width: 70%;
+  margin: auto;
+  padding: 20px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2) ;
+  color: #286fb4;
+}
+
+.router{
+  color: #df4c73;
+  text-decoration: none;
+
+}
+
+.router:hover{
+  color: #286fb4;
+
+}
+
 
 </style>
   
