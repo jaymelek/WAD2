4878c34b8e5c81c2149f7a7a39303ddc86bc80dd
beatbeat8 <beatricegan.2021@smu.edu.sg>
2023-11-02 16:47:34 +0800

    fixes to borrowgame page

--- wad2-maki/src/views/borrowGame.vue
+++ wad2-maki/src/views/borrowGame.vue
@@ -9,7 +9,7 @@
         <div class="row align-items-center justify-content-center m-2"> 
             <div class="col-7 col-md-5 pe-0 pb-4">
                 <!-- <img :src="listing.img" class=" w-75 bg-secondary-subtle rounded" >  -->
-                <div id="gameCarousel" class="carousel slide w-100 bg-secondary-subtle rounded">
+                <div id="gameCarousel" class="carousel slide w-100 carouselBox rounded">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img :src="listing.img" class="d-block w-100" >
@@ -37,32 +37,32 @@
             <div class="container">
                 <form class="row g-3 justify-content-start align-items-center" id="borrowGameForm">
                         <!-- Name -->
-                        <div class="col-md-6">
+                        <div class="col-sm-6">
                             <label for="name1" class="form-label">Matriculated Name:</label>
                             <input type="text" class="form-control inputstl" id="name1" placeholder="Enter Your Full Name" v-model="personName" required>
                         </div>
                         
                         <!-- Telegram Handle -->
-                        <div class="col-md-6">
+                        <div class="col-sm-6">
                             <label for="telegram" class="form-label">Telegram Handle:</label>
                             <input type="text" class="form-control inputstl" id="telegram" name="telegram" v-model="telegram" placeholder="@Username" required>
                         </div>
                 
                         <!-- Borrowing on behalf of themselves or a club -->
-                        <div class="col-md-6">
+                        <div class="col-sm-6">
                             <label class="form-label">Borrowing On Behalf Of</label>
-                            <div class="form-check">
-                                <input class="form-check-input inputstl" type="radio" name="borrowingType" id="self" value="Self" v-model="borrowingBehalf" checked required>
-                                <label class="form-check-label" for="self">Myself</label>
+                            <div class="form-check px-0 pb-1">
+                                <input class="btn-check inputstl" type="radio" name="borrowingType" id="self" value="Self" v-model="borrowingBehalf" checked required>
+                                <label class="btn btn-outline-light w-100 btn-sm" for="self">Myself</label>
                             </div>
-                            <div class="form-check">
-                                <input class="form-check-input inputstl" type="radio" name="borrowingType" id="club" value="Club" v-model="borrowingBehalf" required>
-                                <label class="form-check-label" for="club">My Club</label>
+                            <div class="form-check px-0">
+                                <input class="btn-check inputstl" type="radio" name="borrowingType" id="club" value="Club" v-model="borrowingBehalf" required>
+                                <label class="btn btn-outline-light w-100 btn-sm" for="club">My Club</label>
                             </div>
                         </div>
                 
                         <!-- Email Address -->
-                        <div class="col-md-6">
+                        <div class="col-sm-6">
                             <label for="email1" class="form-label">SMU Email (with Faculty):</label>
                             <input type="email" class="form-control inputstl" id="email1" v-model="email" placeholder="john.2021@scis.smu.edu.sg">
                         </div>
@@ -78,14 +78,14 @@
                         </div>
                 
                         <!-- Loan Start Date -->
-                        <div class="col-md-6">
+                        <div class="col-sm-6">
                             <label for="startDate" class="form-label">Loan Start Date:</label>
                             <input type="date" class="form-control inputstl" id="startDate" name="startDate" required
                                 v-bind:min="minDate()" v-model="loanStartDate"> <!--v-bind:max="maxDate"-->
                         </div>
                 
                         <!-- Loan Return Date -->
-                        <div class="col-md-6">
+                        <div class="col-sm-6">
                             <label for="returnDate" class="form-label">Loan Return Date:</label>
                             <input type="date" class="form-control inputstl" id="returnDate" name="returnDate" required
                                 v-bind:min="loanStartDate" v-bind:max="loanEndDate" v-model="selectedEndDate"> 
@@ -94,43 +94,43 @@
                         <!-- FOR CLUBS ======================================================== -->
 
                             <!-- Club Name -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="clubName" class="form-label">SMU Club Name:</label>
                                 <input type="text" class="form-control inputstl" id="clubName" name="clubName" v-model="clubName" placeholder="Enter Club Name" required>
                             </div>
                 
                             <!-- Club Email Address -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="clubEmail" class="form-label">Club Email:</label>
                                 <input type="email" class="form-control inputstl" id="clubEmail" name="clubEmail" v-model="clubEmail" placeholder="Enter Club Email" required>
                             </div>
 
                             <!-- Club Contact Designation -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="clubContactDesignation" class="form-label">Club Contact Designation:</label>
                                 <input type="text" class="form-control inputstl" id="clubContactDesignation" name="clubContactDesignation" v-model="clubContact" placeholder="e.g., President, Hon Fin, etc." required>
                             </div>
 
                             <!-- Event/Purpose for Borrowing -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="borrowPurpose" class="form-label">Purpose for Borrowing:</label>
                                 <input type="text" class="form-control inputstl" id="borrowPurpose" name="borrowPurpose" v-model="purpose" placeholder="Name of Event, Casual etc" required>
                             </div>
 
                             <!-- Location of Event -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="eventLocation" class="form-label">Location of Event:</label>
                                 <input type="text" class="form-control inputstl" id="eventLocation" name="eventLocation" v-model="location" placeholder="SCIS CR B1-1" required>
                             </div>
 
                             <!-- Who will be Playing -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="personsPlaying" class="form-label">Who will be playing the games?</label>
                                 <input type="text" class="form-control inputstl" id="personsPlaying" name="personsPlaying" v-model="whoPlay" placeholder="Members, Exchange Students..." required>
                             </div>
 
                             <!-- Event Date -->
-                            <div class="col-md-6" v-if="borrowingBehalf == 'Club'">
+                            <div class="col-sm-6" v-if="borrowingBehalf == 'Club'">
                                 <label for="eventDate" class="form-label">When is the event?</label>
                                 <input type="date" class="form-control inputstl" id="eventDate" name="eventDate" :min="loanStartDate" :max="selectedEndDate" required>
                             </div>
@@ -148,9 +148,9 @@
                             If the board games are lost, stolen or damaged, the borrower agrees to promptly notify the Lender representative designated above. In the event of dispute, Strategica has the final say.
                             <br><br>
                             I agree to the clause above, and to ensure the proper use and handling of the loaned board games.
-                            <div width="auto" class="align-items-center">
-                                <input type="checkbox" class="inputstl" id="pdpaClause" name="pdpaClause" required>
-                                <label for="pdpaClause" class="form-label"> &nbsp; Yes</label>
+                            <div width="auto" class="align-items-center form-check-reverse">
+                                <input type="checkbox" class="inputstl form-check-input" id="pdpaClause" name="pdpaClause" required>
+                                <label for="pdpaClause" class="form-check-label"> &nbsp; Yes</label>
                             </div>
                             
                         </div>
@@ -391,7 +391,8 @@ export default {
     }
 
     .inputstl {
-        background-color: rgb(230, 230, 230, 0.3);
+        border: none;
+        /* border-bottom: 2px solid white; */
     }
 
     .marginTop {
@@ -401,5 +402,64 @@ export default {
     .btn-outline-light {
     --bs-btn-hover-color: #7ba7bb;
     --bs-btn-active-color: var(--bs-btn-hover-color);
+    }
+
+    .carouselBox {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+
+    
+    /* Form Styling  ========================================================== */
+    .form-control {
+        border:none;
+        border-bottom: 2px solid white;
+        /* background-color: rgb(255, 255, 255, 0.2);   */
+        background-color: transparent;
+        border-radius: 0px;
+        color: white;
+    }
+
+    .form-control:focus {
+        border-color: #070F5F;
+        box-shadow: none;
+        background-color: transparent;
+        color: white;
+    }
+
+    .form-control::placeholder {
+        color: white;
+        opacity: 0.5;
+    }
+
+    /* Autocomplete Styling for Chrome */
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover, 
+    input:-webkit-autofill:focus,
+    textarea:-webkit-autofill,
+    textarea:-webkit-autofill:hover,
+    textarea:-webkit-autofill:focus,
+    select:-webkit-autofill,
+    select:-webkit-autofill:hover,
+    select:-webkit-autofill:focus {
+    border: none;
+    border-bottom: 2px solid white;
+    -webkit-text-fill-color: white;
+    -webkit-box-shadow: white;
+    transition: background-color 5000s ease-in-out 0s;
+    }
+
+    #startDate, #returnDate, #eventDate {
+        color: white;
+    }
+
+    #pdpaClause:checked{
+        background-color: #070F5F;
+
+    }
+
+    #startDate::-webkit-calendar-picker-indicator,
+    #returnDate::-webkit-calendar-picker-indicator,
+    #eventDate::-webkit-calendar-picker-indicator {
+        filter: invert(1);
     }
 </style>
